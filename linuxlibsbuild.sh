0643264d4a103807c6781fecf285f69a15d57e81
Erik Ogenvik <erik@ogenvik.org>
2020-11-12 19:36:13 +0100

    Add extra checks and syntax cleanup.

--- linuxlibsbuild.sh
+++ linuxlibsbuild.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-MAINDIR=`pwd`
+set -e
+
+MAINDIR=$(pwd)
 mkdir -p dependencies
-cd dependencies
+cd dependencies || exit
 
 # determine which machine we are using
 unameOut="$(uname -s)"
@@ -13,26 +15,25 @@ case "${unameOut}" in
     MINGW*)     machine=MinGw;;
     *)          machine="UNKNOWN:${unameOut}"
 esac
-echo ${machine}
+echo "${machine}"
 
 
 echo "Building boost numeric bindings..."
-gunzip boost-numeric-bindings-20081116.tar.gz
-tar -xvf boost-numeric-bindings-20081116.tar
-cd boost-numeric-bindings
-mkdir -p ${MAINDIR}/include/boost
-mkdir -p ${MAINDIR}/include/boost/numeric
-cp -R boost/numeric/bindings ${MAINDIR}/include/boost/numeric
+tar -xvf boost-numeric-bindings-20081116.tar.gz
+cd boost-numeric-bindings || exit
+mkdir -p "${MAINDIR}"/include/boost
+mkdir -p "${MAINDIR}"/include/boost/numeric
+cp -R boost/numeric/bindings "${MAINDIR}"/include/boost/numeric
 
 cd ..
 
 echo "Building activemq..."
 tar -xvzf activemq-cpp-library-3.9.0-src.tar.gz
-cd activemq-cpp-library-3.9.0
-if ${machine} == "Mac" then
-./configure --prefix=${MAINDIR} --enable-shared 
+cd activemq-cpp-library-3.9.0 || exit
+if [ x"${machine}" = "xMac" ] ; then
+./configure --prefix="${MAINDIR}" --enable-shared
 else
-./configure --prefix=${MAINDIR} --enable-shared --with-apr=/usr/local/opt/apr 
+./configure --prefix="${MAINDIR}" --enable-shared
 fi
 make -j6 install
 
@@ -40,16 +41,16 @@ cd ..
 
 echo "Building ode..."
 tar -xvzf ode-0.12.tar.gz
-cd ode-0.12
-./configure --prefix=${MAINDIR} --enable-shared  --enable-double-precision
+cd ode-0.12 || exit
+./configure --prefix="${MAINDIR}" --enable-shared  --enable-double-precision --disable-demos
 make -j6 install
 
 cd ..
 
 echo "Building google protocol buffers..."
 tar -xvzf protobuf-2.5.0.tar.gz
-cd protobuf-2.5.0
-./configure --prefix=${MAINDIR} --enable-shared 
+cd protobuf-2.5.0 || exit
+./configure --prefix="${MAINDIR}" --enable-shared
 make -j6 install
 
 cd ..
