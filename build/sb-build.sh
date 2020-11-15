1fb8e051d50c3bf2e2d5125b917446b6b6f2a361
Erik Ogenvik <erik@ogenvik.org>
2020-11-15 22:37:55 +0100

    Rearrange a bit.

--- build/sb-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-python sb-automated-build-prepare.py svn://svn.code.sf.net/p/smartbody/code/trunk/build
-
-python scripts/sb-automated-build.py build password -mac-ci nospam@ict.usc.edu
--- build/sb-compile-nix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-mkdir -p bin
-cd bin
-cmake ..
-make
-make install
-cd ..
