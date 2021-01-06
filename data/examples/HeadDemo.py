a98211da5f07e7f08cf316a79f3b62df6216d10e
Erik Ogenvik <erik@ogenvik.org>
2021-01-06 14:05:27 +0100

    Use ChrBrad which seems more complete than ChrMaarten.

--- data/examples/HeadDemo.py
+++ data/examples/HeadDemo.py
@@ -6,7 +6,7 @@ print("|--------------------------------------------|")
 
 # Add asset paths
 scene.addAssetPath('mesh', 'mesh')
-scene.addAssetPath('motion', 'ChrMaarten')
+scene.addAssetPath('motion', 'ChrBrad')
 scene.addAssetPath('script', 'scripts')
 scene.loadAssets()
 
@@ -32,18 +32,57 @@ bradSkeleton = scene.getSkeleton('ChrBrad.sk')
 zebra2Map.applySkeleton(bradSkeleton)
 zebra2Map.applyMotionRecurse('ChrBrad')
 
+characters = ['ChrBrad']
+faces = {}
+# face definition
+for c in characters:
+    face = scene.createFaceDefinition(c)
+    face.setFaceNeutral('ChrBrad@face_neutral')
+    face.setAU(1, "left", c + "@001_inner_brow_raiser_lf")
+    face.setAU(1, "right", c + "@001_inner_brow_raiser_rt")
+    face.setAU(2, "left", c + "@002_outer_brow_raiser_lf")
+    face.setAU(2, "right", c + "@002_outer_brow_raiser_rt")
+    face.setAU(4, "left", c + "@004_brow_lowerer_lf")
+    face.setAU(4, "right", c + "@004_brow_lowerer_rt")
+    face.setAU(5, "both", c + "@005_upper_lid_raiser")
+    face.setAU(6, "both", c + "@006_cheek_raiser")
+    face.setAU(7, "both", c + "@007_lid_tightener")
+    face.setAU(10, "both", c + "@010_upper_lip_raiser")
+    face.setAU(12, "left", c + "@012_lip_corner_puller_lf")
+    face.setAU(12, "right", c + "@012_lip_corner_puller_rt")
+    face.setAU(25, "both", c + "@025_lips_part")
+    face.setAU(26, "both", c + "@026_jaw_drop")
+    face.setAU(45, "left", c + "@045_blink_lf")
+    face.setAU(45, "right", c + "@045_blink_rt")
+
+    face.setViseme("open", c + "@open")
+    face.setViseme("W", c + "@W")
+    face.setViseme("ShCh", c + "@ShCh")
+    face.setViseme("PBM", c + "@PBM")
+    face.setViseme("FV", c + "@FV")
+    face.setViseme("wide", c + "@wide")
+    face.setViseme("tBack", c + "@tBack")
+    face.setViseme("tRoof", c + "@tRoof")
+    face.setViseme("tTeeth", c + "@tTeeth")
+    faces[c] = face
+
+
 # Setting up Brad
 print('Setting up Brad')
 brad = scene.createCharacter('ChrBrad', '')
 bradSkeleton = scene.createSkeleton('ChrBrad.sk')
 brad.setSkeleton(bradSkeleton)
+brad.setFaceDefinition(faces['ChrBrad'])
 brad.createStandardControllers()
 # Deformable mesh
 brad.setVec3Attribute('deformableMeshScale', .01, .01, .01)
-brad.setStringAttribute('deformableMesh', 'ChrMaarten.dae')
+brad.setStringAttribute('deformableMesh', 'ChrBrad.dae')
 
 # Turn on GPU deformable Geometry
 brad.setStringAttribute("displayType", "GPUmesh")
+
+
+
 
 last = 0
 canTime = True
