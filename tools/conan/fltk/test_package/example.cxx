14b5e59eebbbce4aaff1a2aebe50fce0600074f3
Erik Ogenvik <erik@ogenvik.org>
2023-01-15 14:01:52 +0100

    Just compile and link.
    So it can be built on systems where X isn't running.

---
+++ tools/conan/fltk/test_package/example.cxx
@@ -0,0 +1,26 @@
+//	Simple compile and link example.
+//
+// Copyright 2010 Greg Ercolano.
+// Copyright 1998-2010 by Bill Spitzak and others.
+//
+// This library is free software. Distribution and use rights are outlined in
+// the file "COPYING" which should have been included with this file.  If this
+// file is missing or damaged, see the license at:
+//
+//     http://www.fltk.org/COPYING.php
+//
+// Please report all bugs and problems on the following page:
+//
+//     http://www.fltk.org/str.php
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <FL/Fl.H>
+#include <FL/Fl_Double_Window.H>
+#include <FL/Fl_Tree.H>
+
+
+int main(int argc, char *argv[]) {
+  Fl::scheme("gtk+");
+  Fl_Double_Window window(250, 400, "Simple Tree");
+}
