3db717a754d0cc659be251d7189ad12d47f77f38
Erik Ogenvik <erik@ogenvik.org>
2023-01-16 18:54:07 +0100

    Enable builds on Ubuntu 22.04 too.

--- tools/conan/ogre/conanfile.py
+++ tools/conan/ogre/conanfile.py
@@ -22,10 +22,12 @@ class OgreConan(ConanFile):
     short_paths = False
     requires = ["zlib/1.2.13",
                 "bzip2/1.0.8",
-                "freetype/2.11.1",
+                "freetype/2.12.1",
                 "freeimage/3.18.0@smartbody/stable",
                 "assimp/5.2.2",
-                "sdl/2.26.1"]
+                "sdl/2.26.1",
+                "xkbcommon/1.5.0"
+                ]
 
     def system_requirements(self):
         if os_info.is_linux:
