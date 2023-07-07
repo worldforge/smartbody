d09efc8153e67dd970080ebf954be851dd755518
Erik Ogenvik <erik@ogenvik.org>
2023-07-08 00:24:42 +0200

    Fixed conflict.

--- tools/conan/fltk/conanfile.py
+++ tools/conan/fltk/conanfile.py
@@ -65,7 +65,7 @@ class FltkConan(ConanFile):
     def requirements(self):
         self.requires("zlib/1.2.13")
         self.requires("libjpeg/9c")
-        self.requires("libpng/1.6.39")
+        self.requires("libpng/1.6.40")
         if self.settings.os == "Linux":
             self.requires("opengl/system")
             self.requires("glu/system@#ee50fc2ade0e1c0c8ebb3be3ea60e0f5")
